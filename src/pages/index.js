0e5de21eed82dc502a3f449ecbe952881ce4204c
PoisonousEye <PoisonousEye@users.noreply.github.com>
2020-11-27 22:24:30 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -225,16 +225,16 @@ return (
       <h2 css={{textAlign: "center", color: beige}}>Das Team</h2>
       <h3 css={{textAlign: "center"}}>Friseurinnen mit Leidenschaft</h3>
       <p css={{textAlign: "center", maxWidth: "60%", marginBottom: "4em"}}>Haare sind für uns mehr als nur ein Beruf. Sie sind Berufung, Motivation und Lifestyle zugleich. In vielen Fällen genügt ein frischer Schnitt – ganz gleich ob klassisch oder topmodisch – um der Person im Spiegel ganz neuen Glanz zu verleihen und Sie richtig aufleben zu lassen.</p>
-      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em"}}>
+      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em", marginBottom: "2em"}}>
         <TeamCard name="Marita Schindler" titel="Friseurmeisterin" bild="team01" margin={0} />
         <TeamCard name="Christine Siebert" titel="Friseurin /Assistentin" bild="team02" margin={4} />
       </FlexBox>
-      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em"}}>
+      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em", marginBottom: "2em"}}>
         <TeamCard name="Lisa Scheunig" titel="Friseurmeisterin" bild="team03" margin={0} />
         <TeamCard name="Silvia Lickert" titel="Friseurin / Assistentin" bild="team04" margin={4} />
         <TeamCard name="Dann Laur Ott" titel="Friseurin /Gesellin" bild="team05" margin={0} />
       </FlexBox>
-      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em"}}>
+      <FlexBox direction="row" align="flex-start" justify="space-evenly" css={{width: "54em", marginBottom: "2em"}}>
         <TeamCard name="Dind Romano" titel="Friseurin / Gesellin" bild="team06" margin={4} />
         <TeamCard name="Simone Teetz" titel="Friseurin/ Gesellin" bild="team07" margin={0} />
       </FlexBox>
