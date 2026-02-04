5c427a101138c60c04800710d668a8f90066615c
Patrick Hanke <ph@patrickhanke.net>
2026-02-04 12:22:03 +0100

    Update index.ts

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,3 @@
-export {default as Footer} from "./Footer";
 export {KontaktIcon, KontaktIconMobile} from "./KontaktIcon";
 export {default as Preisliste} from "./Preislisten";
 export {default as GoogleMap} from "./GoogleMap";
